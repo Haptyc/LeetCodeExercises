e56b2f3fb35d76de220dccb5193c18ecc9600810
Filip Poborca <filippoborca@Filips-MacBook-Pro.local>
2022-08-28 17:32:38 -0500

    finished reverse integer problem

--- src/august2022/IntegersReversed.java
+++ src/august2022/IntegersReversed.java
@@ -1,7 +1,22 @@
 package august2022;
 
 public class IntegersReversed {
-    public int reversed(int value){
-        String string
+    public static int reversed(int value){
+        int remainder;
+        int reversed = 0;
+        while (value != 0){
+            remainder = value %10;
+            reversed = (reversed * 10) + remainder;
+            value /= 10;
+        }
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        int[] intArray = {123, -123, 120};
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(reversed(intArray[i]));
+        }
     }
 }
